7320d08dc675ae054baae1f57a4fb86c6b4390d2
Raziel23x <raziel23x@gmail.com>
2012-08-14 10:18:35 -0400

    New GUI In Works
    Started working on adding a actual interface to the application which
    will allow me to futher add more options and make this application more
    advanced and help with futher development of apk multi-tools

---
+++ Release.bat
@@ -0,0 +1,215 @@
+@echo off
+setlocal enabledelayedexpansion
+COLOR 0A
+if (%1)==(0) goto skipme
+if (%1) neq () goto adbi
+echo -------------------------------------------------------------------------- >> Release.log
+echo ^|%date% -- %time%^| >> Release.log
+echo -------------------------------------------------------------------------- >> Release.log
+Release 0 2>> Release.log
+
+
+
+:error
+
+:skipme
+cd "%~dp0"
+mode con:cols=90 lines=50
+
+
+
+cls
+
+
+:skipme2
+cls
+ECHO ***************************************************************************************
+ECHO *                               APK Batch Release                                     *
+ECHO ***************************************************************************************
+ECHO *                    Written By Gerald Wayne Baggett Jr {Raziel23x                    *
+ECHO ***************************************************************************************
+ECHO *                           Website: http://apkmultitool.com                          *
+ECHO ***************************************************************************************
+ECHO *           Empowering you to Do Your BEST where ever the path may lead you           *
+ECHO ***************************************************************************************
+ECHO *                                                                                     *
+ECHO *                                                                                     *
+ECHO *                                                                                     * 
+ECHO *                                                                                     *
+ECHO ***************************************************************************************
+ECHO *                                                                                     *
+ECHO *                                                                                     *
+ECHO ***************************************************************************************
+
+
+PAUSE
+:MAINMENU
+cd "%~dp0"
+set menunr=GARBAGE
+cls
+ECHO ***************************************************************************************
+ECHO *                               APK Batch Release                                     *
+ECHO ***************************************************************************************
+ECHO * 0. APK Multi-Tool                                                                   *
+ECHO *    This feature allows you to Zip or tar.gz without packing the .git                *
+ECHO *    Allowing you to release your git project without your private info from github   *
+ECHO ***************************************************************************************
+ECHO * 1. APK-Multi-Tool2                                                                  *
+ECHO *    This feature allows you to Zip or tar.gz without packing the .git                *
+ECHO *    Allowing you to release your git project without your private info from github   *
+ECHO ***************************************************************************************
+ECHO * 2. APK-Multi-Tool-Linux                                                             *
+ECHO *    This feature allows you to Zip or tar.gz without packing the .git                *
+ECHO *    Allowing you to release your git project without your private info from github   *
+ECHO ***************************************************************************************
+ECHO * 3.  COMING SOON                                                                     *
+ECHO *                                                                                     *
+ECHO *                                                                                     *
+ECHO ***************************************************************************************
+ECHO * 4.  COMING SOON                                                                     *
+ECHO *                                                                                     *
+ECHO *                                                                                     *
+ECHO ***************************************************************************************
+ECHO *                                       SETTINGS                                      *
+ECHO ***************************************************************************************
+
+ECHO * 8.   Read Log                                                                       *
+ECHO * 10.  About / Tips / Debug Section                                                   *
+ECHO * 00.  Quit                                                                           *
+ECHO ***************************************************************************************
+SET /P menunr=Please make your decision:
+IF %menunr%==0 (goto MENU00)
+IF %menunr%==1 (goto MENU01)
+IF %menunr%==2 (goto MENU02)
+IF %menunr%==3 (goto MENU03)
+IF %menunr%==4 (goto MENU04)
+IF %menunr%==8 (goto logr)
+IF %menunr%==10 (goto about)
+IF %menunr%==00 (goto quit)
+
+:WHAT
+ECHO ***************************************************************************************
+ECHO *       You went crazy and entered something that wasnt part of the menu options      *
+ECHO ***************************************************************************************
+PAUSE
+goto MAINMENU
+
+:MENU00
+
+cd "%~dp0"
+set menunr=GARBAGE00
+cls
+ECHO ***************************************************************************************
+ECHO *                                APK MULTI-TOOL                                       *
+ECHO ***************************************************************************************
+ECHO *                       Simple Tasks Such As Image Editing                            *
+ECHO ***************************************************************************************
+ECHO * 0    ZIP APK MULTI-TOOL                                                             *
+ECHO * 1    7z  APK MULTI-TOOL                                                             *
+ECHO * 2    tar APK MULTI-TOOL                                                             *
+ECHO * 3    gz  APK MULTI-TOOL                                                             *
+ECHO ***************************************************************************************
+SET /P menunr=Please make your decision:
+IF %menunr%==0 (goto APKZIP)
+IF %menunr%==1 (goto APK7Z)
+IF %menunr%==2 (goto APKTAR)
+IF %menunr%==3 (goto APKGZ)
+
+:WHAT
+ECHO ***************************************************************************************
+ECHO *       You went crazy and entered something that wasnt part of the menu options      *
+ECHO ***************************************************************************************
+PAUSE
+goto MENU00
+:MENU01
+cd "%~dp0"
+set menunr=GARBAGE01
+cls
+ECHO ***************************************************************************************
+ECHO *                               APK MULTI-TOOL2                                       *
+ECHO ***************************************************************************************
+ECHO *                       Simple Tasks Such As Image Editing                            *
+ECHO ***************************************************************************************
+ECHO * 0    ZIP APK MULTI-TOOL2                                                            *
+ECHO * 1    7z  APK MULTI-TOOL2                                                            *
+ECHO * 2    tar APK MULTI-TOOL2                                                            *
+ECHO * 3    gz  APK MULTI-TOOL2                                                            *
+ECHO ***************************************************************************************
+SET /P menunr=Please make your decision:
+IF %menunr%==0 (goto APK2ZIP)
+IF %menunr%==1 (goto APK27Z)
+IF %menunr%==2 (goto APK2TAR)
+IF %menunr%==3 (goto APK2GZ)
+:WHAT
+ECHO ***************************************************************************************
+ECHO *       You went crazy and entered something that wasnt part of the menu options      *
+ECHO ***************************************************************************************
+PAUSE
+goto MENU01
+:MENU02
+cd "%~dp0"
+set menunr=GARBAGE02
+cls
+ECHO ***************************************************************************************
+ECHO *                             APKT MULTI-TOOL Linux                                   *
+ECHO ***************************************************************************************
+ECHO *                       Simple Tasks Such As Image Editing                            *
+ECHO ***************************************************************************************
+ECHO * 0    ZIP APK MULTI-TOOL Linux                                                       *
+ECHO * 1    7z  APK MULTI-TOOL Linux                                                       *
+ECHO * 2    tar APK MULTI-TOOL Linux                                                       *
+ECHO * 3    gz  APK MULTI-TOOL Linux                                                       *
+ECHO ***************************************************************************************
+SET /P menunr=Please make your decision:
+IF %menunr%==0 (goto APKlZIP)
+IF %menunr%==1 (goto APKl7Z)
+IF %menunr%==2 (goto APKlTAR)
+IF %menunr%==3 (goto APKlGZ)
+:WHAT
+ECHO ***************************************************************************************
+ECHO *       You went crazy and entered something that wasnt part of the menu options      *
+ECHO ***************************************************************************************
+PAUSE
+goto MENU02
+:MENU03
+cd "%~dp0"
+set menunr=GARBAGE03
+cls
+ECHO ***************************************************************************************
+ECHO *                                     COMING SOON                                     *
+ECHO ***************************************************************************************
+ECHO *                                                                                     *
+ECHO * 0   BACK TO MAIN MENU                                                               *
+ECHO *                                                                                     *
+ECHO ***************************************************************************************
+SET /P menunr=Please make your decision:
+IF %menunr%==0 (goto MAINMENU)
+
+:WHAT
+ECHO ***************************************************************************************
+ECHO *       You went crazy and entered something that wasnt part of the menu options      *
+ECHO ***************************************************************************************
+PAUSE
+goto MENU03
+:MENU04
+cd "%~dp0"
+set menunr=GARBAGE04
+cls
+ECHO ***************************************************************************************
+ECHO *                                     COMING SOON                                     *
+ECHO ***************************************************************************************
+ECHO *                                                                                     *
+ECHO * 0   BACK TO MAIN MENU                                                               *
+ECHO *                                                                                     *
+ECHO ***************************************************************************************
+SET /P menunr=Please make your decision:
+IF %menunr%==0 (goto MAINMENU)
+
+:WHAT
+ECHO ***************************************************************************************
+ECHO *       You went crazy and entered something that wasnt part of the menu options      *
+ECHO ***************************************************************************************
+PAUSE
+goto MENU04
+
+
